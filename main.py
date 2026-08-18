1b5079fa9e17b6b0371287acc37d8ad66ad94a30
estimation-source <estimation@winsquare.in>
2026-08-18 18:23:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="WIN-SQUARE | Requirement Sheet Engine",
+    layout="wide",
+    page_icon="🪟",
+    initial_sidebar_state="expanded"
+)
 main_page = st.Page("app_home.py", title="Requirement Sheet Engine", icon="🪟", default=True)
 frame_page = st.Page("pages/1_Frame_Details.py", title="Frame Size (WxH) & SQFT", icon="🖼️")
 
